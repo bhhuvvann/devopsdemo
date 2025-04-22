bcddf006a598d54ac0171bd4fd6e9787bcc0dbae
bhhuvvann <bhuvanmahesha@gmail.com>
2025-04-22 13:02:21 +0530

    Update s.py

--- s.py
+++ s.py
@@ -26,7 +26,7 @@ def add():
             num2 = float(request.form['num2'])
             result = num1 + num2
         except (ValueError, KeyError):
-            result = "Invalid inputttt!"
+            result = "Invalid input!"
     return render_template_string(html_form, result=result)
 
 if __name__ == '__main__':
